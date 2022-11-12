724855f366bd783adc63c9c244854c30b5ab0e01
Suhel Keswani <suhelkeswani@gmail.com>
2022-11-12 16:02:43 -0500

    registering users is working

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
 import {getFirestore} from 'firebase/firestore';
-import {getAuth, GoogleAuthProvider} from 'firebase/auth'; //FIX THIS
+import {getAuth} from 'firebase/auth';
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -22,4 +22,3 @@ const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
 export const auth = getAuth(app);
-export const provider = new GoogleAuthProvider(); //FIX THIS
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,25 +1,49 @@
 import React from 'react'
-import {auth, provider} from '../firebase-config'
+import {useState} from 'react'
+import {createUserWithEmailAndPassword} from 'firebase/auth'
+import {auth} from "../firebase-config"
 
 function Login() {
-    const signInWithEmail = () => {
+
+    const [registerEmail, setRegisterEmail] = useState("");
+    const [registerPassword, setRegisterPassword] = useState("");
+    const [loginEmail, setLoginEmail] = useState("");
+    const [loginPassword, setLoginPassword] = useState("");
+
+    const register = async () => {
+        try {
+            const user = await createUserWithEmailAndPassword(auth, registerEmail, registerPassword);
+            console.log(user);
+        } catch (error) {
+            alert(error.message);
+        }
+    }
+
+    const login = async () => {
 
     }
+
+    const logout = async () => {
+
+    }
+
     return(
         <div className="loginPage">
-            <p> Login with Email </p>
-
             <div>
                 <h3> Register User </h3>
-                <input placeholder="Email..."/>
-                <input placeholder="Password..."/>
-                <button> Create User</button>
+                <input placeholder="Email..." onChange={(event) => {setRegisterEmail(event.target.value)}}/>
+                <br></br>
+                <input placeholder="Password..." onChange={(event) => {setRegisterPassword(event.target.value)}}/>
+                <br></br>
+                <button onClick={register}> Create User</button>
             </div>
 
             <div>
                 <h3> Login </h3>
-                <input placeholder="Email..."/>
-                <input placeholder="Password..."/>
+                <input placeholder="Email..." onChange={(event) => {setLoginEmail(event.target.value)}}/>
+                <br></br>
+                <input placeholder="Password..." onChange={(event) => {setLoginPassword(event.target.value)}}/>
+                <br></br>
                 <button> Login</button>
             </div>
 
