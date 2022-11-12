b14792b029e17b529d30448e6e56a94d0ca64b4f
Suhel Keswani <suhelkeswani@gmail.com>
2022-11-12 14:57:42 -0500

    login page front end

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,33 @@
 import React from 'react'
+import {auth, provider} from '../firebase-config'
 
 function Login() {
+    const signInWithEmail = () => {
+
+    }
     return(
-        <div>
-            LOGIN
+        <div className="loginPage">
+            <p> Login with Email </p>
+
+            <div>
+                <h3> Register User </h3>
+                <input placeholder="Email..."/>
+                <input placeholder="Password..."/>
+                <button> Create User</button>
+            </div>
+
+            <div>
+                <h3> Login </h3>
+                <input placeholder="Email..."/>
+                <input placeholder="Password..."/>
+                <button> Login</button>
+            </div>
+
+            <h4> User Logged In: </h4>
+
+            <button> Sign Out </button>
         </div>
-    )
+    );
 }
 
 export default Login;
